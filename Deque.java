82af5295477c417d682a4269f0df08496f5e058a
Tushar Pawar <126349113+Tushar9678@users.noreply.github.com>
2024-02-09 00:03:10 +0530

    Add files via upload

--- Deque.java
+++ Deque.java
@@ -1,35 +1,133 @@
-import java.util.*;
-
 class Deque {
-    private List<Integer> items;
+  static final int MAX = 100; 
+  int arr[]; 
+  int front; 
+  int rear; 
+  int size; 
 
-    public Deque() {
-        this.items = new ArrayList<>();
-    }
-    public boolean isEmpty() {
-        return items.isEmpty();
+  
+  public Deque(int size) {
+    arr = new int[MAX];
+    front = -1; 
+    rear = 0; 
+    this.size = size;
+  }
+
+
+  boolean isFull() {
+    return ((front == 0 && rear == size - 1) || front == rear + 1);
+  }
+
+  boolean isEmpty() {
+    return (front == -1);
+  }
+
+  void insertfront(int key) {
+    if (isFull()) {
+      System.out.println("Overflow");
+      return;
     }
 
-    // Add an item to the front 
-    public void addFront(int item) {
-        items.add(0, item);
+    if (front == -1) { 
+      front = 0;
+      rear = 0;
+    } else if (front == 0) 
+      front = size - 1;
+    else
+      front = front - 1; 
+
+    arr[front] = key; 
+  }
+
+
+  void insertrear(int key) {
+    if (isFull()) {
+      System.out.println("Overflow");
+       return;
     }
 
-    // Add an item to the rear 
-    public void addRear(int item) {
-        items.add(item);
+    if (front == -1) { 
+       front = 0;
+       rear = 0;
+    } else if (rear == size - 1) 
+       rear = 0;
+    else
+      rear = rear + 1; 
+
+    arr[rear] = key; 
+  }
+
+  
+  void deletefront() {
+    if (isEmpty()) {
+      System.out.println("Queue Underflow");
+      return;
     }
 
-    public void display() {
-        System.out.println("Deque: " + items);
+    if (front == rear) { 
+      front = -1;
+      rear = -1;
+    } else if (front == size - 1)
+      front = 0; 
+    else
+      front = front + 1; 
+  }
+
+  
+  void deleterear() {
+    if (isEmpty()) {
+      System.out.println("Underflow");
+      return;
     }
 
-    public static void main(String[] args) {
-        Deque obj = new Deque();
-        obj.addFront(1);
-        obj.addRear(2);
-        obj.addFront(3);
-        
-        obj.display();
+    if (front == rear) { 
+      front = -1;
+      rear = -1;
+    } else if (rear == 0)
+      rear = size - 1; 
+    else
+      rear = rear - 1; 
+  }
+
+  int getFront() {
+    if (isEmpty()) {
+      System.out.println("Underflow");
+      return -1;
     }
+    return arr[front]; 
+  }
+
+  int getRear() {
+    if (isEmpty() || rear < 0) {
+      System.out.println("Underflow");
+      return -1;
+    }
+    return arr[rear]; 
+  }
+
+  public static void main(String[] args) {
+
+    Deque dq = new Deque(4);
+
+    System.out.println("Insert element at rear end : 12 ");
+    dq.insertrear(12);
+
+    System.out.println("insert element at rear end : 14 ");
+    dq.insertrear(14);
+
+    System.out.println("get rear element : " + dq.getRear());
+
+    dq.deleterear();
+    System.out.println("After delete rear element new rear become : " + dq.getRear());
+
+    System.out.println("inserting element at front end");
+    dq.insertfront(13);
+
+    System.out.println("get front element: " + dq.getFront());
+
+    dq.deletefront();
+
+    System.out.println("After delete front element new front become : " + +dq.getFront());
+
+  }
 }
